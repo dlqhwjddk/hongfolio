35e557b4525e203098436310d4be02e06e59593b
hja <dlqhwjddk427@naver.com>
2020-11-30 12:08:15 +0900

    .

--- about.php
+++ about.php
@@ -9,7 +9,7 @@
         <title>HONGFOLIO</title>
         <link rel="stylesheet" href="asset/css/about.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script type="text/javascript" src="asset/js/about.js"></script>
+        <script src="asset/js/about.js"></script>
     </head>
     <body>
         <div class="mobile">
